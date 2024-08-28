2787f45ced8dd3c3e658785b3bf49670128db478
Vlad <banshchikovvl@yandex.ru>
2024-08-28 16:36:32 +0700

    Add files via upload

---
+++ module_1_2.py
@@ -0,0 +1,6 @@
+#Переменные
+task_count = 12
+time_count = 1.5
+name_of_program = 'Python'
+result = (time_count/task_count)
+print('Курс:',name_of_program+', всего задач:',task_count,',затрачено часов:',time_count,',среднее время выполнения',result,'часа.')
